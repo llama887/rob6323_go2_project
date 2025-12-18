036297f58a0de2e126be85d699a51a133d4b1e5d
Alex Tang <at5604@log-2.hpc.nyu.edu>
2025-12-18 08:49:49 -0500

    patch line, add contact sensor setup

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env.py
@@ -119,6 +119,7 @@ class Rob6323Go2Env(DirectRLEnv):
         # add lights
         light_cfg = sim_utils.DomeLightCfg(intensity=2000.0, color=(0.75, 0.75, 0.75))
         light_cfg.func("/World/Light", light_cfg)
+        self.scene.sensors["contact_sensor"] = self._contact_sensor
 
     def _pre_physics_step(self, actions: torch.Tensor) -> None:
         self._actions = actions.clone()
